f247426eaf9fe83273a18602414c49f3473cd76a
Yusuf <38029093+bugnificent@users.noreply.github.com>
2025-02-15 13:30:26 +0300

    Update BrowserUtils.java

--- BrowserUtils.java
+++ BrowserUtils.java
@@ -14,47 +14,7 @@ import java.time.Duration;
 import java.util.Set;
 
 public class BrowserUtils {
-    /*
-    This method will accept int (in seconds)
-    and execute Thread.sleep method for given duration
-    Arg: int second
-     */
-    public static void sleep(int second) {
-        second *= 1000;
-        try {
-            Thread.sleep(second);
-        } catch (InterruptedException e) {
 
-        }
-    }
-
-    public static void switchWindowAndVerify(String expectedInURL, String expectedInTitle) {
-
-        //Return and store all window handles in a Set.
-        Set<String> allWindowHandles = Driver.getDriver().getWindowHandles();
-
-        for (String each : allWindowHandles) {
-
-            Driver.getDriver().switchTo().window(each);
-            System.out.println("Current URL: " + Driver.getDriver().getCurrentUrl());
-
-            if (Driver.getDriver().getCurrentUrl().contains(expectedInURL)) {
-                break;
-            }
-        }
-
-        //5. Assert:Title contains “Etsy”
-        String actualTitle = Driver.getDriver().getTitle();
-        Assert.assertTrue(actualTitle.contains(expectedInTitle));
-    }
-
-    public static void verifyTitle(String expectedTitle) {
-        Assert.assertEquals(Driver.getDriver().getTitle(), expectedTitle);
-    }
-
-    public static void verifyTitleContains(String expectedInTitle) {
-        Assert.assertTrue(Driver.getDriver().getTitle().contains(expectedInTitle));
-    }
 
     public BrowserUtils findUsername(String testCase) {
         String filePath = ConfigurationReader.getProperty("filePath");
@@ -171,9 +131,9 @@ public class BrowserUtils {
             boolean found = false;
 
             for (Row row : sheet) {
-                Cell testCaseCell = row.getCell(3); // Test case numarasının olduğu sütun
+                Cell testCaseCell = row.getCell(3);
                 if (testCaseCell != null && testCaseCell.getStringCellValue().equals(testCase)) {
-                    Cell statusCell = row.getCell(12); // Status olduğu sütun
+                    Cell statusCell = row.getCell(12);
                     if (statusCell != null) {
                         statusCell.setCellValue("FAILED");
                         found = true;
@@ -197,56 +157,7 @@ public class BrowserUtils {
         return this;
 
     }
-
-
-    /*
-    This method accepts WebElement target,
-    and waits for that WebElement not to be displayed on the page
-     */
-    public static void waitForInvisibilityOf(WebElement target) {
-        //Create the object of 'WebDriverWait' class, and set up the constructor args
-        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(10));
-
-        //use the 'wait' object with the proper syntax to create explicit wait conditions.
-        wait.until(ExpectedConditions.invisibilityOf(target));
-    }
-
-//    public static void waitForText(WebElement page, String searchKeyword){
-//        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(10));
-//        wait.until(ExpectedConditions.attributeContains(page,"data-replicated-value", searchKeyword));
-//    }
-
-    public static void waitForText(WebElement element, String attribute) {
-        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(10));
-        wait.until(driver -> {
-            String value = element.getAttribute(attribute);
-            System.out.println("Mevcut Değer: " + value); // Debug için
-            return value != null && !value.isEmpty();
-        });
-    }
-
-    public static void verifyUrl() {
-        Driver.getDriver().getCurrentUrl();
-    }
-
-
-    /*
-    This method accepts String title,
-    and waits for that Title to contain given String value.
-     */
-    public static void waitForTitleContains(String title) {
-        //Create the object of 'WebDriverWait' class, and set up the constructor args
-        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(20));
-
-        //use the 'wait' object with the proper syntax to create explicit wait conditions.
-        wait.until(ExpectedConditions.titleContains(title));
-    }
-
-    public static void waitForTitle(String title, int time) {
-
-        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(time));
-
-        wait.until(ExpectedConditions.titleIs(title));
-    }
-
+ 
 }
+
+
